a65769b2697201347094d6316c58d29a23606f87
Thor Andreassen <ta@toggle.be>
2022-06-06 13:10:29 +0200

    Use an output buffer for printing

--- tmd5/md5.c
+++ tmd5/md5.c
@@ -11,8 +11,10 @@ int main(void)
   MHASH td;
   unsigned char buffer[32];
   unsigned char hash[16]; /* enough size for MD5 */
+  unsigned char outb[33];
   unsigned char hex[] = "0123456789abcdef";
 
+  outb[32] = '\0';
   while(fgets(buffer, 32, stdin) != NULL) {
 
     td = mhash_init(MHASH_MD5); if (td == MHASH_FAILED) exit(1);
@@ -22,11 +24,13 @@ int main(void)
     mhash_deinit(td, hash);
     
     for(i = 0; i < 16; i++) {
-      putchar(hex[ hash[i] >>   4 ]);
-      putchar(hex[ hash[i] & 0x0f ]);
+      outb[2*i  ] = hex[ hash[i] >>   4 ]; 
+      outb[2*i+1] = hex[ hash[i] & 0x0f ];
+      /*putchar(hex[ hash[i] >>   4 ]);*/
+      /*putchar(hex[ hash[i] & 0x0f ]);*/
     }
 
-    printf("\t%s", buffer);
+    printf("%s\t%s", outb, buffer);
   }
 
   exit(0);
