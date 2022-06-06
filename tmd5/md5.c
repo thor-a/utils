47c97d7a2128c71a347087761e0d1597f7a38581
Thor A <ta@toggle.be>
2022-06-06 11:07:56 +0000

    Use #define for buffer sizes

--- tmd5/md5.c
+++ tmd5/md5.c
@@ -5,23 +5,26 @@
 #include <stdlib.h>
 #include <string.h>
 
+#define BUFSZ 1024
+
 int main(void)
 {
   int i;
   MHASH td;
-  unsigned char buffer[32];
-  unsigned char hash[16]; /* enough size for MD5 */
+
+  unsigned char buffer[BUFSZ]; /* line length limit */
+  unsigned char hash[16];      /* enough size for MD5 */
   unsigned char hex[] = "0123456789abcdef";
 
-  while(fgets(buffer, 32, stdin) != NULL) {
+  while(fgets(buffer, BUFSZ, stdin) != NULL) {
 
     td = mhash_init(MHASH_MD5); if (td == MHASH_FAILED) exit(1);
 
-    mhash(td, &buffer, strlen(buffer)-1);
+    mhash(td, &buffer, strnlen(buffer)-1, BUFSZ);
 
     mhash_deinit(td, hash);
     
-    for(i = 0; i < 16; i++) {
+    for(i = 0; i <= 15; i++) {
       putchar(hex[ hash[i] >>   4 ]);
       putchar(hex[ hash[i] & 0x0f ]);
     }
